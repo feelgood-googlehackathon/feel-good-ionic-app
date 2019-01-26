dbcd241175c780a49aae1d8b511105a327e3f41c
TheGloriousGenesis <TheGloriousGenesis@users.noreply.github.com>
2019-01-26 17:14:06 +0000

    Update contact.ts

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,20 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {SharedServiceProvider} from "../../providers/shared-service/shared-service";
-
+import * as data from "../../assets/inspirationalquotesjson.txt";
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-  public favourites: any[];
-
+  quotes: any[];
+  
   constructor(public navCtrl: NavController, private sharedService: SharedServiceProvider) {
   }
 
   ionViewDidLoad() {
     // console.log("hi from quotes");
-    // this.favourites = this.sharedService.getQuotes();
+    this.quotes = this.sharedService.getQuotes();
   }
-
+  
+  processQuotes() {
+    this.data.forEach((obj) => {
+      let quoteCardObj = {'text' : obj.text, "liked": false, "bookmark":false};
+      this.quotes.push(quoteCardObj);
+    });
+    this.sharedService.setQuoteCardObjectsArray(this.quotes);
+  }
+ 
+  toggleLike(quoteCard: any) {
+    if (quoteCard['liked'] === true) {
+      quoteCard['liked'] = false;
+    } else {
+      quoteCard['liked] = true;
+      this.sharedService.setStoredFavourites(quoteCard);          
+  }
+    
 }
+
+                        
